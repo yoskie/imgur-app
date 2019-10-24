5abc89396a58cf0c871a34741836803e732232b6
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-10-24 22:32:04 +0900

    feat: appheader added with semantic-ui styling

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AppHeader></AppHeader>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+};
+</script>
