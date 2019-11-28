80da808f9c9532cec18cc37dbda09985b9a5752c
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-29 01:52:00 +0900

    feat: authhandler added and env syntax revised

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,15 +1,12 @@
 import qs from 'qs';
 
-const CLIENT_ID = 'process.env.VUE_APP_CLIENT_ID';
-const ROOT_URL = 'process.env.VUE_APP_ROOT_URL'
-
 export default {
   login() {
     const querystring = {
-      client_id: CLIENT_ID,
+      client_id: process.env.VUE_APP_CLIENT_ID,
       response_type: 'token'
     };
 
-    window.location = `${ROOT_URL}/oath2.authorize?${qs.stringify(querystring)}`;
+    window.location = `${process.env.VUE_APP_ROOT_URL}/oath2.authorize?${qs.stringify(querystring)}`;
   }
 }
