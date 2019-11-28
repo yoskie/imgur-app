b2cb6416aa57b0fe2ceb7a639d0b9d5e5e784b3d
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-29 01:21:38 +0900

    feat: auth modules added with api file

---
+++ src/store/modules/auth.js
@@ -0,0 +1,32 @@
+import api from '../../api/imgur';
+
+const state= {
+  token: null
+};
+
+const getters = {
+  isLoggedIn: state => !!state.token
+};
+
+const actions = {
+  login: () => {
+    api.login();
+  },
+  logout: ({ commit }) => {
+    commit('setToken', null);
+  }
+};
+
+const mutations = {
+  setToken: () => (state, token) => {
+    state.token = token;
+  }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
+
